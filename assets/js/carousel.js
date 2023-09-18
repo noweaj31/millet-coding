e955fbc8b10ed03745a316623452e7e8ddef0c52
noweaj31 <noweajeel31@gmail.com>
2023-09-18 22:27:50 +0900

    feat:슬라이드

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,8 +1,13 @@
 "use strict";
 
+// main-img
 const carouselWrap = document.querySelector(".main-img .img-area");
-const carouselItem = document.querySelector(".main-img > img");
-const totalCalrousels = document.querySelectorAll(".main-img > img").length;
+const carouselItem = document.querySelector(".main-img .img-area img");
+const prevButton = document.querySelector(".btn-prev");
+const nextButton = document.querySelector(".btn-next");
+const totalCalrousels = document.querySelectorAll(
+  ".main-img .img-area img"
+).length;
 const calrouselWidth = carouselItem.clientWidth;
 let currentCarousel = 0;
 let autocarouselInterval;
@@ -10,7 +15,6 @@ let autocarouselInterval;
 const rotateCarousel = (direction) => {
   currentCarousel += direction;
   const scrollX = currentCarousel * calrouselWidth;
-
   carouselWrap.scrollTo({
     left: scrollX,
     behavior: "smooth",
@@ -23,5 +27,37 @@ const startAutoCarousel = () => {
   }, 3000);
 };
 
-rotateCarousel();
-startAutoCarousel(); //자동시작실행
+nextButton.addEventListener("click", () => {
+  rotateCarousel(1);
+});
+
+prevButton.addEventListener("click", () => {
+  rotateCarousel(-1);
+});
+
+startAutoCarousel();
+
+// best-item
+const itemWrap = document.querySelector("main-content .item-wrap");
+const Item = document.querySelectorAll("main-content .item-1");
+const totalItems = document.querySelectorAll("main-content .item-1").length;
+const ItemWidth = Item.clientWidth;
+let currentItem = 0;
+let autoItemInterval;
+
+const rotateItem = (direction) => {
+  currentCarousel += direction;
+  const scrollX = currentCarousel * ItemWidth;
+  carouselWrap.scrollTo({
+    left: scrollX,
+    behavior: "smooth",
+  });
+};
+
+const startAutoItem = () => {
+  autoItemInterval = setInterval(() => {
+    rotateItem(1);
+  }, 3000);
+};
+
+startAutoItem();
